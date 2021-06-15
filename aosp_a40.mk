eca92d139dccb55ad08173974aa4cba5812c683b
kevios12 <kerschbaumer.kevin2@gmail.com>
2021-06-15 19:58:19 +0200

    a40: lineage -> aosp

---
+++ aosp_a40.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, device/samsung/a40/full_a40.mk)
+
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+PRODUCT_NAME := aosp_a40
--- lineage_a40.mk
+++
@@ -1,6 +0,0 @@
-$(call inherit-product, device/samsung/a40/full_a40.mk)
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-PRODUCT_NAME := lineage_a40
