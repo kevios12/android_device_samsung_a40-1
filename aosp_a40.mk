7225eb3b0b82e558e266fcdd8e7eb6b8d2787f6d
kevios12 <kerschbaumer.kevin2@gmail.com>
2021-06-16 02:30:58 +0200

    a40: add Blur

--- aosp_a40.mk
+++ aosp_a40.mk
@@ -4,3 +4,6 @@ $(call inherit-product, device/samsung/a40/full_a40.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_NAME := aosp_a40
+
+# Blur
+TARGET_USES_BLUR := true
