15b592e67d015805e98f603ee700eca2ec8ad259
kevios12 <kerschbaumer.kevin2@gmail.com>
2021-06-19 20:08:07 +0200

    a40: set bootanimation resolution

--- aosp_a40.mk
+++ aosp_a40.mk
@@ -5,5 +5,8 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_NAME := aosp_a40
 
+# Bootanimation resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Blur
 TARGET_USES_BLUR := true
