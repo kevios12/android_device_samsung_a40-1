c5627dfdb885f113dcef3e19abb1d58c802cd4e4
kevios12 <kerschbaumer.kevin2@gmail.com>
2021-06-19 20:08:39 +0200

    a40: set bootanimation resolution

--- lineage_a40.mk
+++ lineage_a40.mk
@@ -5,5 +5,8 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_a40
 
+# Bootanimation resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Blur
 TARGET_USES_BLUR := true
