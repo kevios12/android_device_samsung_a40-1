04b27bf660b8e7329a6382ac4bfd13fe8cf4ccec
kevios12 <kerschbaumer.kevin2@gmail.com>
2021-07-11 11:51:42 +0200

    a40: Spark-Ify

--- spark_a40.mk
+++ spark_a40.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/samsung/a40/full_a40.mk)
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common spark stuff.
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_a40
+PRODUCT_NAME := spark_a40
 
 # Bootanimation resolution
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -14,3 +14,5 @@ TARGET_USES_BLUR := true
 # GAPPS if vendor/gms or vendor/gapps exists
 TARGET_GAPPS_ARCH := arm64
 WITH_GAPPS := true
+
+
